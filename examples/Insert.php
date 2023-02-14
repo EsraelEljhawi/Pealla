e4ef8d3bdf1dc5e793cdd85c9d5f26d38dc4be65
Rahma Abubaker Said <itsrahmasaid.official@gmail.com>
2023-02-14 11:28:00 +0200

    InsertTodb

---
+++ examples/Insert.php
@@ -0,0 +1,25 @@
+<?php
+global $link;
+
+include 'db_conect.php';
+
+$UserName = mysqli_real_escape_string($link, $_REQUEST['UserName']);
+$UserEmail = mysqli_real_escape_string($link, $_REQUEST['Email']);
+$Phone = mysqli_real_escape_string ($link, $_REQUEST['Phone']);
+$Password = mysqli_real_escape_string($link, $_REQUEST['Password']);
+
+$sql = "INSERT INTO `sing`(`UserName`,`Email`, `Phone`, `Password`) VALUES ('$UserName','$UserEmail', '$Phone', '$Password')";
+
+if (mysqli_query($link, $sql)){
+header("location: dashboard.html");
+}
+else
+{
+echo "ERROR: This user already exists just Login NOW";
+}
+// close connection
+
+
+mysqli_close($link);
+
+?>
